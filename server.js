6a6e721cf7ef3ed3b8c3f7e1cb4cbe0205ec5fcd
Nate Bowditch <nhbowditch@gmail.com>
2017-10-07 11:49:16 -0400

    fixes port issue for real?

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const sanitizer = require('sanitizer');
 const app = express();
 
-app.set('port', (process.env.port || 5000))
+app.set('port', (process.env.PORT || 5000))
 app.set('views', './dist');
 
 app.engine('html', require('ejs').renderFile);
